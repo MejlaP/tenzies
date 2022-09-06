f232632aa4d2e12362fb10939e8ee9de50fea74a
mejlap <miloshp8@gmail.com>
2022-09-06 09:07:09 +0200

    dice won

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,57 @@ import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
 
 
 function App() {
-
-  //initial allNewDice()
+  // initial state diceNumbers with (allNewDice())
   const [diceNumbers, setDiceNumbers] = React.useState(allNewDice())
 
-  // it generates and returns ten random numbers
+  // initial state tenzies represents whether the user has won the game yet or not
+  const [tenzies, setTenzies] = React.useState(false)
+
+  // effect that runs every time the `dice` state array changes
+  // checks if all dice are held, and all dice have the same value
+  React.useEffect(() => {
+    const allDiceHeld = diceNumbers.every(diceNumber => diceNumber.isHeld)
+    const firstPositionValue = diceNumbers[0].value
+    const allSameValue = diceNumbers.every(diceNumber => diceNumber.value === firstPositionValue)
+    if (allDiceHeld && allSameValue) {
+      console.log('You won')
+    }
+  }, [diceNumbers])
+
+  // function for generating new dice as object
+  function generateNewDice() {
+    return {
+      value: Math.floor(Math.random() * 6 + 1),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
+  // it generates and returns ten random numbers as objects(generateNewDice())
   function allNewDice() {
     let randomNumbers = []
-
     for (let i = 0; i < 10; i++) {
-      const randomNumber = {
-        value: Math.floor(Math.random() * 6 + 1),
-        isHeld: false,
-        id: nanoid()
-      }
-      randomNumbers.push(randomNumber)
+      randomNumbers.push(generateNewDice())
     }
     return randomNumbers
   }
 
   // function that takes `id` as a parameter
-  // to pass that function down to each instance of the Die component so when each one is clicked, it logs its own unique ID property
+  // to flip the`isHeld` property on the object in the array that was clicked, based on the 'id'prop passed into the function
   function holdDice(id) {
-    console.log(id)
+    setDiceNumbers(prevDiceNumbers =>
+      prevDiceNumbers.map(diceNumber =>
+        diceNumber.id === id ? { ...diceNumber, isHeld: !diceNumber.isHeld } : diceNumber
+      ))
   }
 
-  // function for generate a new array of numbers and set the `dice` state to that new array
+  // function for checking if diceNumber.isHeld, else it generates new dice
   function rollDice() {
-    setDiceNumbers(allNewDice())
+    setDiceNumbers(prevDiceNumbers =>
+      prevDiceNumbers.map(diceNumber =>
+        diceNumber.isHeld ? diceNumber : generateNewDice()
+      ))
   }
-
 
   // create diceNumbersElements from Die components
   const diceNumbersElements = diceNumbers.map(diceNumber => <Die key={diceNumber.id} id={diceNumber.id} value={diceNumber.value} isHeld={diceNumber.isHeld} holdDice={holdDice} />)
